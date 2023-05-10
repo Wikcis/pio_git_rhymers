cf6dfca0be6d0c7decd2c9cfe4f24b5e3a1cffc5
Wikcis <wiktor.ciszewski7734@gmail.com>
2023-05-10 23:56:55 +0200

    17. Dodanie szkieletu dokumentacji do klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * The DefaultCountingOutRhymer class represents a stack of numbers.
+ */
 public class DefaultCountingOutRhymer {
 
     private static final int NUMBERS_AMOUNT = 12;
@@ -8,30 +11,47 @@ public class DefaultCountingOutRhymer {
     private final int[] numbers = new int[NUMBERS_AMOUNT];
 
     private int total = INITIAL_TOTAL;
-
+    /**
+     * @return number of elements in stack
+     */
     public int getTotal() {
         return total;
     }
-
+    /**
+     * inserts value to stack
+     * @param in - value to insert
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
-
+    /**
+     * Checks if the stack is empty.
+     * @return true if stack is empty and false if stack is not empty.
+     */
     public boolean callCheck() {
         return total == ERROR;
     }
-
+    /**
+     * Checks if the stack is full.
+     * @return true if stack is full and false if it is not.
+     */
     public boolean isFull() {
         return total == NUMBERS_AMOUNT+INITIAL_TOTAL;
     }
-
+    /**
+     * Returns the value of the top element on the stack without removing it.
+     * @return The value of the top element on the stack.
+     */
     protected int peekaboo() {
         if (callCheck())
             return ERROR;
         return numbers[total];
     }
-
+    /**
+     * Returns the value of the top element on the stack and removes it.
+     * @return The value of the top element on the stack.
+     */
     public int countOut() {
         if (callCheck())
             return ERROR;
